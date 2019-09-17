eaf704224d85f8d58cba2c423c852e275ffa41a9
Tigity <mika.thurler@gmail.com>
2019-09-17 17:28:00 -0400

    ici c'est 'the best'

--- TheBest.java
+++ TheBest.java
@@ -1,7 +1,16 @@
 
 public class TheBest {
-    
+
     public static void main(String[] args) {
+
+      System.out.println("71 (master) $ j TheBest");
+      System.out.println();
+
+      // copy paste le code ci dessous et mettez votre info
+      System.out.println("Bonjour, je m<appelle Mika Thurler");
+      System.out.println("Mon numéro d'étudiant est 300056543");
+      System.out.println("un peu sur moi : j'aime skier et lire.");
+      System.out.println();
 
     }
 }
